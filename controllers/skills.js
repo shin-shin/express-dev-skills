05bfe6c5276c3d8e7f762ad4dfa54224f44ead0f
Irene Tokareva <longhorn09@gmail.com>
2019-11-19 19:52:57 -0600

    add a new skill and delete

--- controllers/skills.js
+++ controllers/skills.js
@@ -2,19 +2,56 @@ const FakeData = require('../models/skill');
 
 module.exports = {
     index,
-    show
+    show,
+    new: newSkill,
+    create: createSkill,
+    delete: deleteSkill,
+    update
 }
 function index(req, res) {
+    console.log("index action");
+
     res.render('skills/index', {
         skills: FakeData.getAll(),
         title: "List of Students"
     })
 }
 function show(req, res) {
-    console.log(`request is ${FakeData.getOne(req.params.id.student)}`);
+    console.log("show action");
     res.render('skills/show', {
-        studentSkills: FakeData.getOne(req.params.id).skills,
+        title: `Dev Skills of ${FakeData.getOne(req.params.id).student}`,
+        studentId: req.params.id,
         student: FakeData.getOne(req.params.id).student,
-        title: `Skills of ${FakeData.getOne(req.params.id).student}`
+        studentSkills: FakeData.getOne(req.params.id).skills,
     })
+}
+function newSkill(req, res) {
+    console.log("newSkill action");
+    res.render('skills/new', {
+        title: `Add ${FakeData.getOne(req.params.id).student}'s New Skill`,
+        studentId: req.params.id,
+        student: FakeData.getOne(req.params.id).student,
+    })
+}
+
+function createSkill(req, res) {
+    if(req.body.skill) {
+        let obj = FakeData.getOne(req.params.id)
+        obj.skills.push({skill: req.body.skill, acquired: req.body.acquired})
+        res.redirect(`/skills/${req.params.id}`)
+    } else {
+        res.redirect(`/skills/new/${req.params.id}`)
+    }
+}
+
+
+function deleteSkill(req, res){
+    let obj = FakeData.getOne(req.params.id)
+    obj.skills.splice(req.params.idx, 1);
+    res.redirect(`/skills/${req.params.id}`)
+}
+
+function update(req, res){
+    
+    res.redirect(`/skills/${req.params.id}`)
 }
--- routes/skills.js
+++ routes/skills.js
@@ -5,6 +5,9 @@ var skillsCtrl = require('../controllers/skills')
 
 /* GET users listing. */
 router.get('/', skillsCtrl.index);
+router.get('/:id/new', skillsCtrl.new);
+router.post('/:id/new', skillsCtrl.create)
+router.get('/:id/delete/:idx', skillsCtrl.delete);
 router.get('/:id', skillsCtrl.show);
 
 module.exports = router;
